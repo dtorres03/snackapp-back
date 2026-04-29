6d37199c890aa12713a50da5f82c34ce5308ae57
Gustavo Salas <lordtavo27@gmail.com>
2026-04-29 18:34:39 -0500

    feat: configure Django project settings with PostgreSQL, JWT authentication, and Swagger documentation

--- core/settings.py
+++ core/settings.py
@@ -54,6 +54,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOW_ALL_ORIGINS = True # Para desarrollo, permite peticiones desde Next.js
+
+
 
 # Application definition
 
@@ -69,6 +72,7 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    'corsheaders',
     'users',
     'videos',
     'interactions'
@@ -77,6 +81,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
